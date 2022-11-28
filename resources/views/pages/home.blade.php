53ed2587226d348107a6294c65f5960c96433241
JorgeMiguelDelgado <41923835+JorgeMiguelDelgado@users.noreply.github.com>
2022-11-27 20:16:22 -0400

    template

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.default')
+ 
+@section('title', 'Pagina Principal')
+ 
+
+ 
+@section('content')
+    <div class="right_col" role="main">
+        <div class="row" style="display: inline-block;">
+    <img src="{{asset('images/izi.png')}}" alt="">
+        </div>
+</div>
+@endsection
